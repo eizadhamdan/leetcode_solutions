12e5a07f41bd25f6d153d3ad26091d5053649803
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-05 16:05:19 +0530

    a few math questions

---
+++ 43-multiply_strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
+
+        """ 
+        Alternative-
+
+        a = 0
+        for i in range(len(num1)):
+            a = a*10 + int(num1[i])
+        
+        b = 0
+        for i in range(len(num2)):
+            b = b*10 + int(num2[i])
+
+        return str(a * b)
+        """
