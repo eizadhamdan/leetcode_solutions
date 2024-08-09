b81f2549cd586958fc3a120fa32bfe32e774541d
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-08-09 22:58:03 +0530

    valid parentheses

---
+++ AlgoMap/Stacks/20-valid_parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::unordered_map<char, char> hashmap = {{')', '('}, {'}', '{'}, {']', '['}};
+        std::stack<char> stk;
+
+        for (char c : s) {
+            if (hashmap.find(c) == hashmap.end()) {
+                stk.push(c);
+            } 
+            else {
+                if (stk.empty() || stk.top() != hashmap[c]) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+
+        return stk.empty();
+    }
+};
